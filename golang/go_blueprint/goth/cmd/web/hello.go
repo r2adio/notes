4774faf405be0cbac968339641573bfee2e680f0
GauraVashisth <miya.musashi.moto1645@gmail.com>
2025-07-21 22:43:49 +0530

    go_blueprint/goth: add imformative comments for hello.go and understand templ components

--- golang/go_blueprint/goth/cmd/web/hello.go
+++ golang/go_blueprint/goth/cmd/web/hello.go
@@ -6,16 +6,18 @@ import (
 )
 
 func HelloWebHandler(w http.ResponseWriter, r *http.Request) {
-	err := r.ParseForm()
+	err := r.ParseForm() //r.ParseForm(): parse encoded form data; and loads entire form into memory
 	if err != nil {
 		http.Error(w, "Bad Request", http.StatusBadRequest)
+		return //stops the execution
 	}
 
 	name := r.FormValue("name")
-	component := HelloPost(name)
-	err = component.Render(r.Context(), w)
+	component := HelloPost(name)           //component
+	err = component.Render(r.Context(), w) //component rendering
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusBadRequest)
 		log.Fatalf("Error rendering in HelloWebHandler: %e", err)
+		return
 	}
 }
